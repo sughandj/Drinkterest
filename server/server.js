6ff18eab848f6d8d451ca1d2663e8f0913a2d972
Jai Sughand <jaisughand@gmail.com>
2018-02-02 22:21:24 -0500

    Added ENV PORT for Heroku

--- server/server.js
+++ server/server.js
@@ -209,6 +209,6 @@ app.get('/lcboapi/products*', function (req, res, next) {
 
 // Listen
 
-app.listen(5000, function () {
-  console.log('App listening on port 5000');
+app.listen(process.env.PORT || 5000, function () {
+  console.log('App running');
 });
