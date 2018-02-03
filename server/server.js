401e00117b39fb880af9531351de50e72f4cf032
Jai Sughand <jaisughand@gmail.com>
2018-02-02 23:00:44 -0500

    Heroku build

--- server/server.js
+++ server/server.js
@@ -11,10 +11,6 @@ var NedbStore = require('nedb-session-store')(session);
 
 app.use(express.static(path.join(__dirname,'../client/build')));
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
 app.use(session({
   secret: 'topsecret',
   resave: true,
